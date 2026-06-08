f6af11406d01c88425132a6e6548851eaae3599f
santhosh0118 <santhoshtadikimalla@gmail.com>
2026-06-08 13:24:30 +0530

    numbers

--- intro/numberdatatype/numberdatatype.js
+++
@@ -1,7 +0,0 @@
-function onClick(){
-    debugger;
-    var firstNumber=number(document.getElementById)("txtFirstNumber").value);
-
-var secondnumber=Number(document.getElementById)("txtsecondtNumber").value);
-var sum=firstNumber+secondnumber;
-}
